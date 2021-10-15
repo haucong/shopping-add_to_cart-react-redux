f90cb4af3460dde42be79ca84b79ff3e6ed23481
haucong <ngoconghau65@gmail.com>
2021-10-15 08:04:44 +0700

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+
+function App() {
+  return (
+    <div className='grid-container'>
+      <header className='App-header'>
+        <a href='/'>React shopping cart</a>
+      </header>
+      <main>Poduct List</main>
+      <footer>All right is reserved</footer>
+    </div>
+  )
+}
+
+export default App;
